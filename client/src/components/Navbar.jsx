231ec01af916912f1bd1c1659e6f035df1b917bd
khaled <khaledb.yahya@gmail.com>
2023-09-08 21:41:40 +0100

    navabr

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,13 +1,36 @@
-//import React from "react";
+import {useState} from "react";
 import logo from "../assets/pizza_logo.png";
+import { Link } from "react-router-dom";
+import ReorderIcon from '@mui/icons-material/Reorder';
+import "../styles/Navbar.css";
 
 export default function Navbar() {
+  const [openMenu, setOpenMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setOpenMenu(!openMenu);
+  }
+
   return (
     <div className="navbar">
-      <div className="leftSide">
-        <img className="logo" src={logo} alt="" />
+      <div className="leftSide" id={openMenu ? "open" : "close"}>
+        <img src={logo} alt="" /><span>By Sinatra</span>
+        <div className="hiddenLinks">
+          <Link to="/">Home</Link>
+          <Link to="/menu">Menu</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
+        </div>
       </div>
-      <div className="rightSide"></div>
+      <div className="rightSide">
+        <Link to="/">Home</Link>
+        <Link to="/menu">Menu</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+        <button onClick={toggleMenu}>
+          <ReorderIcon />
+        </button>
+      </div>
     </div>
   );
 }
