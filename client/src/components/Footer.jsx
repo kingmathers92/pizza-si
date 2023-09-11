4aa8f7677f8edbbed3a9c8828f577317bdf5d919
khaled <khaledb.yahya@gmail.com>
2023-09-11 22:35:47 +0100

    footer tweak

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,8 +1,9 @@
 import InstagramIcon from "@mui/icons-material/Instagram";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import FacebookIcon from "@mui/icons-material/Facebook";
-import EmailIcon from '@mui/icons-material/Email';
-import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
+import EmailIcon from "@mui/icons-material/Email";
+import LocalPhoneIcon from "@mui/icons-material/LocalPhone";
+import LanguageIcon from "@mui/icons-material/Language";
 import "../styles/Footer.css";
 
 export default function Footer() {
@@ -12,19 +13,44 @@ export default function Footer() {
         <a href="https://www.instagram.com/pizzasitn/">
           <InstagramIcon />
         </a>
-        <a href="https://twitter.com/your-twitter-profile" target="_blank" rel="noopener noreferrer">
+        <a
+          href="https://twitter.com/your-twitter-profile"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <TwitterIcon />
         </a>
-        <a href="https://www.facebook.com/PizzaSi/" target="_blank" rel="noopener noreferrer">
+        <a
+          href="https://www.facebook.com/PizzaSi/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <FacebookIcon />
         </a>
-        <a href="mailto:pizzasi.sinatra@gmail.com" target="_blank" rel="noopener noreferrer">
+        <a
+          href="mailto:pizzasi.sinatra@gmail.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <EmailIcon />
         </a>
       </div>
       <div className="info">
-        <p> &copy; 2023 pizzasi.com</p>
-        <LocalPhoneIcon/><p>99 888 044</p>
+        <LanguageIcon />
+        <p>pizzasi.com</p>
+        <LocalPhoneIcon />
+        <p>99 888 044</p>
+        <p>
+          Created by:{" "}
+          <a
+            href="https://www.khaledbenyahya.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Khaled Ben Yahya
+          </a>
+          <span>&nbsp; &copy; 2023</span>
+        </p>
       </div>
     </div>
   );
