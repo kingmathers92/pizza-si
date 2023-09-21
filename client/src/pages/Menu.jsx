5da8887bcd783b54e3e59408a942a55a5bd5a72e
khaled <khaledb.yahya@gmail.com>
2023-09-21 21:11:36 +0100

    menu background

--- client/src/pages/Menu.jsx
+++ client/src/pages/Menu.jsx
@@ -3,12 +3,14 @@ import MenuItem from "../components/MenuItem";
 import "../styles/Menu.css";
 import { motion } from "framer-motion";
 import { useTranslation } from "react-i18next";
+import Background from "../assets/pizza_back2.jpg";
 
 export default function Menu() {
   const { t } = useTranslation();
   return (
     <motion.div
       className="menu"
+      style={{ backgroundImage: `url(${Background})` }}
       initial={{ width: 0 }}
       animate={{ width: "100%" }}
       exit={{ x: window.innerWidth, transition: { duration: 0.1 } }}
