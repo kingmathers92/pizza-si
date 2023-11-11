1883860fbd17fce407a0a2bc03ae58dc3ca14e8e
khaled <khaledb.yahya@gmail.com>
2023-11-11 22:01:16 +0100

    ids items

--- client/src/utils/MenuList.js
+++ client/src/utils/MenuList.js
@@ -8,52 +8,57 @@ import Makloub from "../assets/makloub.jpg";
 import Sweet from "../assets/sweet.jpg";
 
 export const MenuList = [
+  { id: 1, name: "Pepperoni Pizza", image: Pepperoni, price: 16 },
   {
-    name: "Pepperoni Pizza",
-    image: Pepperoni,
-    price: 16,
-  },
-  {
+    id: 2,
     name: "Vegeterian Pizza",
     image: Vegeterian,
     price: 17,
   },
   {
+    id: 3,
     name: "Mexican Pizza",
     image: Mexicain,
     price: 15.5,
   },
   {
+    id: 4,
     name: "4 Seasons Pizza",
     image: "4seasons",
     price: 16,
   },
   {
+    id: 5,
     name: "Jombon Pizza",
     image: Jombon,
     price: 18,
   },
   {
+    id: 6,
     name: "Napolitana Pizza",
     image: Napolitana,
     price: 22,
   },
   {
+    id: 7,
     name: "Baguette Farci",
     image: Baguette,
     price: 14,
   },
   {
+    id: 8,
     name: "Makloub",
     image: Makloub,
     price: 7,
   },
   {
+    id: 9,
     name: "Sweet Chocolate Pizza",
     image: Sweet,
     price: 12,
   },
   {
+    id: 10,
     name: "Tunisian Sandwich",
     image: "",
     price: 5,
