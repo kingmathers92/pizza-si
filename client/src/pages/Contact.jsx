559d3fdfc5054af45b5816ea1f5d492f777baf0f
khaled <khaledb.yahya@gmail.com>
2023-11-26 16:26:30 +0100

    contact form finished

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Location from "../assets/location.jpg";
 import "../styles/Contact.css";
 import { motion } from "framer-motion";
@@ -5,6 +6,11 @@ import { useTranslation } from "react-i18next";
 
 export default function Contact() {
   const { t } = useTranslation();
+
+  const [userName, setUserName] = useState("");
+  const [userEmail, setUserEmail] = useState("");
+  const [userMessage, setUserMessasge] = useState("");
+
   return (
     <motion.div
       className="contact"
@@ -19,27 +25,44 @@ export default function Contact() {
       <div className="rightSide">
         <h1>{t("title")}</h1>
 
-        <form id="contact-form" method="POST">
+        <form
+          id="contact-form"
+          action="https://getform.io/f/9789724e-994f-4ea8-8b4e-a328bedd28a7"
+          method="POST"
+        >
           <label htmlFor="name">{t("fullName")}</label>
           <input
             name="name"
             placeholder={t("fullNamePlaceholder")}
             type="text"
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+            required
           />
           <label htmlFor="email">{t("email")}</label>
           <input
             name="email"
             placeholder={t("emailPlaceholder")}
             type="email"
+            value={userEmail}
+            onChange={(e) => setUserEmail(e.target.value)}
+            required
           />
           <label htmlFor="message">{t("message")}</label>
           <textarea
             rows="6"
             placeholder={t("messagePlaceholder")}
             name="message"
+            value={userMessage}
+            onChange={(e) => setUserMessasge(e.target.value)}
             required
           ></textarea>
-          <button type="submit">{t("sendMessage")}</button>
+          <button
+            disabled={!userName || !userEmail || !userMessage}
+            type="submit"
+          >
+            {t("sendMessage")}
+          </button>
         </form>
       </div>
     </motion.div>
