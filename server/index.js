bdae13fb6f30b27fdd05b37d6a0174277a8c9436
khaled <khaledb.yahya@gmail.com>
2023-11-30 16:42:57 +0100

    cashondelivery feature done

--- server/index.js
+++ server/index.js
@@ -4,6 +4,15 @@ require("dotenv").config();
 const stripe = require("stripe")(process.env.STRIPE_SECRET_TEST);
 const cors = require("cors");
 
+const admin = require("firebase-admin");
+
+const serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: process.env.DB_URL,
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
@@ -29,6 +38,29 @@ app.post("/payment", cors(), async (req, res) => {
     console.log("Error", error);
     res.status(500).json({
       message: "Payment failed",
+      success: false,
+    });
+  }
+});
+
+app.post("/create-order", cors(), async (req, res) => {
+  const orderDetails = req.body;
+
+  try {
+    const db = admin.database();
+    const ordersRef = db.ref("orders");
+
+    const newOrderRef = ordersRef.push();
+    await newOrderRef.set(orderDetails);
+
+    res.status(200).json({
+      message: "Order created successfully",
+      success: true,
+    });
+  } catch (error) {
+    console.log("Error", error);
+    res.status(500).json({
+      message: "Failed to create order",
       success: false,
     });
   }
