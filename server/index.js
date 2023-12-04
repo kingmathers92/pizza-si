7568e9b57e2c47130ea843d97976a15aacba61a7
khaled <khaledb.yahya@gmail.com>
2023-12-04 21:18:26 +0100

    serviceAccount

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 
 const admin = require("firebase-admin");
 
-const serviceAccount = require("./serviceAccountKey.json");
+const serviceAccount = require("./serviceAccountKey.js");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
